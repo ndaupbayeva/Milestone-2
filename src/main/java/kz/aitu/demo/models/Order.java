47d67a67079a690a67f5b5eea1079df9c5e5f41a
alish766 <230633@astanait.edu.kz>
2024-03-01 22:18:18 +0600

    add order model

---
+++ src/main/java/kz/aitu/demo/models/Order.java
@@ -0,0 +1,16 @@
+package kz.aitu.demo.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+@Data
+@Entity
+@Table(name="orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private boolean finished;
+    private String date;
+    private String time;
+    private String shipping_method; }
