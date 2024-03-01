52dfee58d4462d338dfc46246f4d1a14a4480314
User <231583@astanait.edu.kz>
2024-03-01 22:05:42 +0600

    I created a model dishes

--- src/main/java/kz/aitu/demo/models/Dish.java
+++ src/main/java/kz/aitu/demo/models/Dish.java
@@ -2,19 +2,14 @@ package kz.aitu.demo.models;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.Generated;
-
 @Data
 @Entity
-@Table(name="employees")
-
-public class Employee {
+@Table(name="dishes")
+public class Dish {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
-    private String surname;
-    private String position;
-    private int age;
-    private boolean gender;
+    private int difficulty;
+    private int cost;
 }
