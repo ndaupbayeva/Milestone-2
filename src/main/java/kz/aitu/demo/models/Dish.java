f2999a256f094daec58bc8025f74774b64137e60
alish766 <230633@astanait.edu.kz>
2024-03-01 21:41:22 +0600

    add dish model

---
+++ src/main/java/kz/aitu/demo/models/Dish.java
@@ -0,0 +1,15 @@
+package kz.aitu.demo.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+@Data
+@Entity
+@Table(name="dishes")
+public class Dish {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private int difficulty;
+    private int cost;
+}
